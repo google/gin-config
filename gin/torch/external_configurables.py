d0137e57d1acc423ab374efeb0c521919b27e831
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2019-05-24 15:09:58 +0300

    change copyright in new modules
    Signed-off-by: Giorgos Paraskevopoulos <geopar@central.ntua.gr>

--- gin/torch/external_configurables.py
+++ gin/torch/external_configurables.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2019 Georgios Paraskevopoulos.
+# Copyright 2019 The Gin-Config Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
